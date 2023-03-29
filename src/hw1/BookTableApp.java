3bd63f20f123ce451b46a066255859f18381cbcc
hacker228973 <kodiy9@gmail.com>
2023-03-29 18:14:04 +0200

    ust

---
+++ src/hw1/BookTableApp.java
@@ -0,0 +1,12 @@
+package hw1;
+
+import java.util.Scanner;
+
+public class BookTableApp {
+    public static void main(String[] args) {
+        System.out.println("Правда хотите забронировать стол?");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        System.out.println("Отлично столик зарегестрирован");
+    }
+}
